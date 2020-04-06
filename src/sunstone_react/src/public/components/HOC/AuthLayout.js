f25503e84f67cf1c04d752025395b6b1b1e8c4a6
Jorge Lobo <jlobo@opennebula.systems>
2020-04-06 17:18:39 +0200

    M #~: fix component auth
    Signed-off-by: Jorge Lobo <jlobo@opennebula.systems>

--- src/sunstone_react/src/public/components/HOC/AuthLayout.js
+++ src/sunstone_react/src/public/components/HOC/AuthLayout.js
@@ -13,7 +13,7 @@
 /* limitations under the License.                                             */
 /* -------------------------------------------------------------------------- */
 
-import React, { Component, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { requestData, removeStoreData, findStorageData } from '../../utils';
@@ -21,48 +21,35 @@ import constants from '../../constants';
 import components from '../router/endpoints';
 import { setUser } from '../../actions';
 
-class AuthLayout extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      show: false
-    };
-    this.redirectToLogin = this.redirectToLogin.bind(this);
-  }
+const AuthLayout = ({ children, history, changeName }) => {
+  const [show, setShow] = useState(false);
 
-  componentDidMount() {
+  const redirectToLogin = () => {
+    const { jwtName } = constants;
+    removeStoreData(jwtName);
+    const { login } = components;
+    history.push(login.path);
+  };
+
+  useEffect(() => {
     const { jwtName, endpointsRoutes } = constants;
-    const { changeName } = this.props;
     if (findStorageData && findStorageData(jwtName)) {
       requestData(endpointsRoutes.userInfo).then(response => {
         if (response && response.data && response.data.USER) {
           const { USER: userInfo } = response.data;
-          this.setState({ show: true });
+          setShow(true);
           changeName(userInfo.NAME);
         } else {
-          this.redirectToLogin();
+          redirectToLogin();
         }
       });
     } else {
-      this.redirectToLogin();
+      redirectToLogin();
     }
-  }
+  }, []);
 
-  redirectToLogin() {
-    const { jwtName } = constants;
-    const { history } = this.props;
-    removeStoreData(jwtName);
-    const { login } = components;
-    history.push(login.path);
-  }
-
-  render() {
-    const { show } = this.state;
-    const { children } = this.props;
-    const render = show ? <Fragment>{children}</Fragment> : <Fragment />;
-    return render;
-  }
-}
+  return show ? <Fragment>{children}</Fragment> : <Fragment />;
+};
 AuthLayout.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
@@ -72,25 +59,13 @@ AuthLayout.propTypes = {
   history: PropTypes.shape({
     push: PropTypes.func
   }),
-  match: PropTypes.shape({
-    params: PropTypes.shape({}),
-    path: PropTypes.string,
-    isExact: PropTypes.bool,
-    url: PropTypes.string
-  }),
   changeName: PropTypes.func
 };
 
 AuthLayout.defaultProps = {
-  children: [],
+  children: '',
   history: {
     push: () => undefined
-  },
-  match: {
-    params: {},
-    path: '',
-    isExact: false,
-    url: ''
   },
   changeName: () => undefined
 };
